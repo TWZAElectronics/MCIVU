8e7a529371bc78a51363690c6f7a5c496c847965
ldanster <lebohangdanster@gmail.com>
2014-07-24 18:36:11 +0200

    Reading values for the keyboard reading

--- mcivu/mcivu.ino
+++ mcivu/mcivu.ino
@@ -1,7 +1,13 @@
+
+int keyboardReadPin = 0;
+int keyboardReading = 0;
+
 void setup() {
-  
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  keyboardReading = analogRead(keyboardReadPin);
+  Serial.println(keyboardReading);
+  delay(1000);
 }
