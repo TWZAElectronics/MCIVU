b20bce20916bd9fb76245a68adca1b198b0e7bed
ldanster <lebohangdanster@gmail.com>
2014-07-24 18:58:14 +0200

    Adding commands to each switch read

--- mcivu/mcivu.ino
+++ mcivu/mcivu.ino
@@ -11,3 +11,19 @@ void loop() {
   Serial.println(keyboardReading);
   delay(1000);
 }
+
+String getCommand(keyboardReading) {
+  if (keyboardReading > 140 && keyboardReading < 145) {
+    return 'CMD_READY';
+  } else if (keyboardReading > 507 && keyboardReading < 511) {
+    return 'CMD_MODE';
+  } else if (keyboardReading > 336 && keyboardReading < 340) {
+    return 'CMD_DEPLOY';
+  } else if (keyboardReading > 250 && keyboardReading < 255) {
+    return 'CMD_DEPLOY_TO';
+  } else if (keyboardReading > 199 && keyboardReading < 204) {
+    return 'CMD_BACK';
+  } else if (keyboardReading > 166 && keyboardReading < 170) {
+    return 'CMD_FORWARD';
+  }
+}
