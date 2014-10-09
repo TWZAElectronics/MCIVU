3d9afb151e635138f2c7247d803056b47884d29b
Charles Boadu <cboadu@thoughtworks.com>
2014-10-09 19:25:58 +0200

    Cleans up text parsing algorithm and handshaking feature

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Keyboard Protocol
     CMD_DEPLOY_TO - deploy to the NEXT STAGE in a pipeline
          CMD_BACK - scroll back through a stage or pipeline depending on current mode
       CMD_FORWARD - scroll forward through a stage or pipeline depending on current mode
+         CMD_EXIT - stop application
 
 
 Display Protocol
@@ -29,11 +30,11 @@ Display Protocol
     Example 1:
     DSP:LN1:SCR0:CMXR:SAMPLE TEXT 1
     Display the text 'SAMPLE TEXT 1' text on line 1 and turn the 1st LED indicator red.
-        
+
     Example 2:
     DSP:LN2:SCR1:CMXA:SAMPLE TEXT 2
     Scroll the text 'SAMPLE TEXT 2' on line 2 and make all LEDs green
-        
+
     Example 3:
     DSP:LN1:SCR1:CMXGGGY:SAMPLE TEXT 3
     Scroll the text 'SAMPLE TEXT 3' on line 1, turn the 1st, 2nd, 3rd and 4th LEDs Green, Green, Green and Yellow respectively
--- mcivu/mcivu.ino
+++ mcivu/mcivu.ino
@@ -3,50 +3,59 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int keyboardReadPin = 0;
-String keyboardReading = "";
+char *keyboardReading;
+int backlightPin = 13;
 
 int numberOfPipelines = 5;
 char DELIMETER = ':';
-String inCommand = "";
 
+char *inData;
 
 void setup() {
   lcd.begin(16, 2);
+  pinMode(backlightPin, OUTPUT);
+  digitalWrite(backlightPin, HIGH);
   Serial.begin(9600);
 }
 
+char *readSerialPort() {
+  char inData[100];
+  char inChar;
+  byte index = 0;
 
-void loop() {
-  keyboardReading = getKeyboardReading(analogRead(keyboardReadPin));
-
-  if (keyboardReading != "CMD_READY" && keyboardReading != "UNKNOWN") {
-    sendCommand(keyboardReading);
+  while(Serial.available() > 0) {
+    if(index < 99) {
+      inChar = Serial.read();
+      inData[index] = inChar;
+      index++;
+      inData[index] = '\0';
+    }
   }
 
-  inCommand = Serial.read();
-  if (inCommand != "") {
-    sendCommand("ACK");
-    processCommand(keyboardReading);
-  }
-
-  delay(1000);
+  return inData;
 }
 
 
-String getKeyboardReading(int keyboardReading) {
+char *getKeyboardReading(int keyboardReading) {
   if (keyboardReading > 140 && keyboardReading < 145) {
     return "CMD_READY";
-  } else if (keyboardReading > 507 && keyboardReading < 511) {
+  }
+  else if (keyboardReading > 507 && keyboardReading < 511) {
     return "CMD_MODE";
-  } else if (keyboardReading > 336 && keyboardReading < 340) {
+  }
+  else if (keyboardReading > 336 && keyboardReading < 340) {
     return "CMD_DEPLOY";
-  } else if (keyboardReading > 250 && keyboardReading < 255) {
+  }
+  else if (keyboardReading > 250 && keyboardReading < 255) {
     return "CMD_DEPLOY_TO";
-  } else if (keyboardReading > 199 && keyboardReading < 204) {
+  }
+  else if (keyboardReading > 199 && keyboardReading < 204) {
     return "CMD_BACK";
-  } else if (keyboardReading > 166 && keyboardReading < 170) {
+  }
+  else if (keyboardReading > 166 && keyboardReading < 170) {
     return "CMD_FORWARD";
-  } else {
+  }
+  else {
     return "UNKNOWN";
   }
 }
@@ -54,27 +63,21 @@ String getKeyboardReading(int keyboardReading) {
 
 // fact: command is retried 10 times until an 'ACK' message is received from the connecting peripheral
 void sendCommand(String command) {
-  int counter = 0;
-
-  do {
-    Serial.println(command);
-    counter += 1;
-    delay(100);
-  } while (Serial.read() != "ACK" && counter <= 10)
+  Serial.println(command);
+  delay(1500);
 }
 
-
-void serialEvent() {
-  String data =(String) Serial.read();
-  processCommand(data);
-  delay(1000);
-}
+//void serialEvent() {
+//  String data =(String) Serial.read();
+//  processCommand(data);
+//  delay(1000);
+//}
 
 
 // DSP
 void processCommand(String inputCommand) {
   if (inputCommand.startsWith("DSP")) {
-    processDisplayCommand(inputCommand.substring(3));
+    processDisplayCommand(inputCommand.substring(4));
   }
 }
 
@@ -92,35 +95,37 @@ void processDisplayCommand(String displayCommand) {
     delimeterPosition = displayCommand.indexOf(DELIMETER);
 
     if (delimeterPosition != -1) {
-        displaySubCommand = displayCommand.substring(0, delimeterPosition);
+      displaySubCommand = displayCommand.substring(0, delimeterPosition);
 
-        if (displaySubCommand.startsWith("LN")) {
-          lineNumber = getLineNumber(displaySubCommand);
-          if (lineNumber == -1)
-            return;
-        } else if (displaySubCommand.startsWith("SCR")) {
-          scroll = getScroll(displaySubCommand);
-          if (scroll == -1)
-            return;
-        } else if (displaySubCommand.startsWith("CMX")) {
-          colourCommand = displaySubCommand;
-        }
+      if (displaySubCommand.startsWith("LN")) {
+        lineNumber = getLineNumber(displaySubCommand);
+        if (lineNumber == -1)
+          return;
+      }
+      else if (displaySubCommand.startsWith("SCR")) {
+        scroll = getScroll(displaySubCommand);
+        if (scroll == -1)
+          return;
+      }
+      else if (displaySubCommand.startsWith("CMX")) {
+        colourCommand = displayCommand.substring(3, displayCommand.indexOf(DELIMETER));
+      }
 
-        displayCommand = displayCommand.substring(delimeterPosition+1, displayCommand.length());
+      displayCommand = displayCommand.substring(delimeterPosition+1, displayCommand.length());
     }
-  } while(delimeterPosition >=0);
+  }
+  while(delimeterPosition >= 0);
 
-  displayText(displaySubCommand, lineNumber -1, scroll);
+  displayText(displayCommand, lineNumber -1, scroll);
   setColour(colourCommand);
 }
-
 
 int getLineNumber(String lineNumberCommand) {
   if (lineNumberCommand.startsWith("LN")) {
     char lineNumber = lineNumberCommand.charAt(2);
 
     if (!isAlpha(lineNumber)) {
-      return (int)lineNumber;
+      return ((int)lineNumber - 48);
     }
   }
 
@@ -130,10 +135,10 @@ int getLineNumber(String lineNumberCommand) {
 
 int getScroll(String scrollCommand) {
   if (scrollCommand.startsWith("SCR")) {
-    char scrollValue = scrollCommand.charAt(2);
+    char scrollValue = scrollCommand.charAt(3);
 
     if (!isAlpha(scrollValue)) {
-      return (int)scrollValue;
+      return ((int)scrollValue - 48);
     }
   }
 
@@ -153,7 +158,8 @@ void setColour(String colourMatrixCommand) {
       for (int i=0; i<numberOfPipelines; i+=1) {
         setColour('G', i);
       }
-    } else {
+    }
+    else {
       for (int i=0; i<colourMatrixLength; i+=1) {
         setColour(colourMatrix.charAt(i), i);
       }
@@ -178,14 +184,27 @@ void lightsOut() {
 
 void displayText(String text, int lineNumber, int scroll) {
   lcd.setCursor(0, lineNumber);
+  lcd.print(text);
+  delay(2000);
 
-  if(scroll == 0) {
-    lcd.print(text);
-  } else {
+  if(scroll == 1) {
     for (int positionCounter = 0; positionCounter < text.length(); positionCounter++) {
       lcd.scrollDisplayLeft();
-      delay(150);
+      delay(500);
     }
+
+    delay(1000);
   }
+
+  lcd.setCursor(0, lineNumber);
+  lcd.print(text);
 }
 
+void loop() {
+  keyboardReading = getKeyboardReading(analogRead(keyboardReadPin));
+  if(strcmp(keyboardReading, "CMD_READY") != 0 && strcmp(keyboardReading, "UNKNOWN") != 0) {
+    sendCommand(keyboardReading);
+    inData = readSerialPort();
+    processCommand(inData);
+  }
+}
